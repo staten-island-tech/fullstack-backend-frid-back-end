41355e889ad176092a6ab0e1c6b4437b6e08e68f
DanBel23 <71675993+DanBel23@users.noreply.github.com>
2022-01-16 15:34:03 -0500

    1/16/22 3:33 PM EDT

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express(); 
 
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+    req.RequestTime = new Date().toISOString();
+    next();
+})
 
 const dummyInfo = [
     {
@@ -14,17 +20,28 @@ const dummyInfo = [
 ];
 
 
-
-app.get('/api/v1/posts', (req, res) => {
+const getAllPosts =  (req, res) => {
     res.status(200).json({
         status: 'success',
         data: {
-            dummyInfo
+            dummyInfo,
+            time: req.RequestTime
         }
     })
-});
+};
 
-app.post('/api/v1/posts', (req, res) => {
+
+const getPost = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+          id: req.params.id
+        }
+    })
+}
+
+
+const createPost = (req, res) => {
     dummyInfo.push(req.body);
     res.status(201).json({
         status: "success",
@@ -32,7 +49,10 @@ app.post('/api/v1/posts', (req, res) => {
          dummyInfo: req.body
         }
     })
-})
+}
+app.route('/api/v1/posts/:id').get(getPost);
+
+app.route('/api/v1/posts').get(getAllPosts).post(createPost);
 
 const port = 3000;
 app.listen(port, () =>{
