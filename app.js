5bb4ded281dd4a7873ca6eb978f754f8354ec8e5
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2022-02-07 12:29:41 -0500

    2/7/22 12:30 PM

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
 const postRouter = require("./routes/postRoutes");
-const app = express(); 
+const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
-    req.RequestTime = new Date().toISOString();
-    next();
-})
+  req.RequestTime = new Date().toISOString();
+  next();
+});
 
-
-app.use('/api/v1/posts', postRouter);
-app.use('/api/v1/users', userRouter);
-
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
