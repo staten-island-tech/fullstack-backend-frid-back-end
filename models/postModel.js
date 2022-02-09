3c7bae9d389ffcad2459dde1c3e1cae43c5da615
L_General_RohrReich <71799901+AlexanderKokhno@users.noreply.github.com>
2022-02-08 21:29:31 -0500

    minor edits

--- models/postModel.js
+++ models/postModel.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A post must have a name'],
-    },
-    totalLikes: {
-        type: Number,
-        default: 0
-    },
-    totalDislikes: {
-        type: Number,
-        default: 0
-    },
-    songs: [String],
+  name: {
+    type: String,
+    required: [true, "A post must have a title"],
+    trim: true,
+  },
+  totalLikes: {
+    type: Number,
+    default: 0,
+  },
+  totalDislikes: {
+    type: Number,
+    default: 0,
+  },
+  songs: [String],
 
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
-const Post = mongoose.model('Posts', postSchema);
+const Post = mongoose.model("Posts", postSchema);
 
-module.exports = Post; 
+module.exports = Post;
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-dotenv.config({ path: './config.env'});
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app');
+const app = require("./app");
 
 const DB = process.env.DATABASE.replace(
-    '<password>',
-    process.env.DATABASE_PASSWORD
+  "<password>",
+  process.env.DATABASE_PASSWORD
 );
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true 
-}).then(() => {
-    console.log("Connected");
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DataBase!");
+  });
 
-
- 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>{
-    console.log(`We are live on port ${port}`);
+app.listen(port, () => {
+  console.log(`We are live on port ${port}`);
 });
 
+mongoose.connection.on("error", (err) => {
+  console.log(`${err.message}`);
+});
