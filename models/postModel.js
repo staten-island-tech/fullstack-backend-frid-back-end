ed6ff280732e53eee239c2eb04300a7d75131534
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2022-02-11 12:28:14 -0500

    expanded postmodel

--- models/postModel.js
+++ models/postModel.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
-  name: {
+  postName: {
     type: String,
     required: [true, "A post must have a title"],
     trim: true,
+  },
+  songAmount: {
+    type: Number, 
+    required: [true, "No empty playlists Permitted"],  
+  },
+  song1: {
+    songName: {
+      type: String,
+      required: [true, "A song must have a name"],
+      // trim: true,
+    },
+    artist: {
+      type: String,
+    },
+    duration: {
+      type: Number,
+      required: [true, "The song must be more tha 0 seconds"],
+      default: 0,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
   totalLikes: {
     type: Number,
@@ -13,11 +36,25 @@ const postSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
-  songs: [String],
-
-  createdAt: {
-    type: Date,
-    default: Date.now(),
+  comments: {
+    totalComments: {
+      type: Number,
+      default: 0,
+    },
+    comment1: {
+      commentContent: {
+        type: String,
+        default: "type comment here",
+      },
+      commentLikes: {
+        type: Number,
+        default: 0,
+      },
+      userID: {
+        type: Number,
+        required: [true, "User ID?"],
+      },
+    }
   },
 });
 const Post = mongoose.model("Posts", postSchema);
