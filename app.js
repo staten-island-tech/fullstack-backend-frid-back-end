93333476279926f86a900d4b547b5b21db03fc52
DanBel23 <71675993+DanBel23@users.noreply.github.com>
2022-02-13 17:23:02 -0500

    2/13/21 5:22 PM

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./appError');
+const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRoutes");
 const postRouter = require("./routes/postRoutes");
 const app = express(); 
@@ -15,11 +17,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/users', userRouter);
 
+
+
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `${req.originalUrl} not found`
-    });
+    next(new AppError(`${req.originalUrl} not found`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
