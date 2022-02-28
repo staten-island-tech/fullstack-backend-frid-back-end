348129b6dfa6c8fad29352d060b89c4a34aa1a26
DanBel23 <71675993+DanBel23@users.noreply.github.com>
2022-02-27 21:09:25 -0500

    2/27/21 9:09 PM

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,57 @@
-module.exports = ((error, req, res, next) => {
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'error';
+const AppError = require("../appError");
 
+const handleCastErrorDB = error => {
+    console.log('we got');
+    const message = `Invalid ${error.path}: ${error.value}`
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = error => {
+    const value = error.errormsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}. Please use a different value!`;
+    return new AppError(message, 400);
+};
+
+const sendErrorDev = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        error: error,
+        message: error.message,
+        stack: error.stack
+    });
+};
+
+const sendErrorProd = (error, res) => {
+    if (error.isOperational) {
     res.status(error.statusCode).json({
         status: error.status,
         message: error.message
     });
+    } else {
+        console.error('ERROR', error);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        })
+    }
+}
+const handleValidationErrorDB = error => {
+    const errors = Object.values(error.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
+
+module.exports = ((error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+       sendErrorDev(error, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError')   error = handleValidationErrorDB(error);
+       sendErrorProd(error, res);
+    }
 });
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config({ path: './config.env'});
 
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log("UNCAUGHT EXCEPTION! SERVER IS SHUTTING DOWN");  
+    process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -20,7 +26,14 @@ mongoose.connect(DB, {
 
  
 const port = process.env.PORT || 3000;
-app.listen(port, () =>{
+const server = app.listen(port, () =>{
     console.log(`We are live on port ${port}`);
 });
 
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log("UNHANDLED REJECTION! SERVER IS SHUTTING DOWN");
+    server.close(() => {
+        process.exit(1);
+    });
+});
