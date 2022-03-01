80fcf285451fc00bdd5833aa88eefe5966f9734b
StanleyRabkin <stanley.rabkin23@sitechhs.com>
2022-03-01 12:28:53 -0500

    12:32 PM 03/01/22
    Cors <3

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-const AppError = require('./appError');
+const express = require("express");
+const morgan = require("morgan");
+const AppError = require("./appError");
 const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRoutes");
 const postRouter = require("./routes/postRoutes");
-const app = express(); 
+const cors = require("cors");
+const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
-    req.RequestTime = new Date().toISOString();
-    next();
-})
+  req.RequestTime = new Date().toISOString();
+  next();
+});
 
-app.use('/api/v1/posts', postRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
-
-
-app.all('*', (req, res, next) => {
-    next(new AppError(`${req.originalUrl} not found`, 404));
+app.all("*", (req, res, next) => {
+  next(new AppError(`${req.originalUrl} not found`, 404));
 });
 
 app.use(globalErrorHandler);
