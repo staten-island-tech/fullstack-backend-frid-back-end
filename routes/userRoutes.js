9862839c6416c86ecbce44d625abc3b8932b1e9c
DanBel23 <71675993+DanBel23@users.noreply.github.com>
2022-03-30 20:24:19 -0400

    3/30/21 8:24 PM
    New route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const userCon = require("../controllers/userController");
 const express = require("express");
+const Spot = require("../test");
 
 const router = express.Router();
 
@@ -8,6 +9,8 @@ const router = express.Router();
 router.route('/:id').get(userCon.getUser).patch(userCon.updateUser).delete(userCon.deleteUser);
 
 router.route('/').get(userCon.getAllUsers);
+
+router.route('/spotify').get(Spot.setToken);
 
 router.post('/create', userCon.createUser);
 
