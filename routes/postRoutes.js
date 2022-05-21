55077f72328a86b65df087b77bd714b43b6a0abd
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2022-05-21 09:39:25 -0400

    5/21/22 9:39 AM
    hopefully fixed bacckend issue, if issue persists please report

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,10 +4,14 @@ const { auth, requiresAuth } = require("express-openid-connect");
 
 const router = express.Router();
 
-router.route('/:id').get(postCon.getPost).patch(requiresAuth(), postCon.updatePost).delete(requiresAuth(), postCon.deletePost);
+router
+  .route("/:id")
+  .get(postCon.getPost)
+  .patch(postCon.updatePost)
+  .delete(postCon.deletePost);
 
-router.route('/').get(postCon.getAllPosts);
+router.route("/").get(postCon.getAllPosts);
 
-router.post('/create', requiresAuth(), postCon.createPost);
+router.post("/create", postCon.createPost);
 
 module.exports = router;
