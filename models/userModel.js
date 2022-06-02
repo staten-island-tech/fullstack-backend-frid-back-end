d45f67555bdd32e5ba979dc6600494d467b8a7bc
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2022-06-02 12:20:43 -0400

    6/2/22 12:20PM

--- models/userModel.js
+++ models/userModel.js
@@ -1,55 +1,58 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: [true, "A account must have a user name"],
-    trim: true,
-  },
-  AccountCreatedAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  email: {
-    type: String,
-    required: [true, "A account must have an email"],
-    trim: true,
-    lowercase: true,
-    validate: [validator.isEmail, "Please enter a valid email address"],
-  },
-  password: {
-    type: String,
-    required: [true, "Please enter a password"],
-    minLength: 8,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please confirm your password"],
-    minLength: 8,
-  },
-  following: [
-    {
-      userID: {
-        type: Number,
-        required: [false, "User ID?"],
-      },
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "A account must have a user name"],
+      trim: true,
     },
-  ],
-  totalFollowing: {
-    type: Number,
-    default: 0,
-  },
-  followers: [
-    {
-      userName: {
-        type: String,
-        required: [false, "User ID?"],
-      },
-    userSavedIDs: [
+    AccountCreatedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    email: {
+      type: String,
+      required: [true, "A account must have an email"],
+      trim: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minLength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      minLength: 8,
+    },
+    following: [
       {
-        postID: {
-          type: String,
+        userID: {
+          type: Number,
+          required: [false, "User ID?"],
         },
+      },
+    ],
+    totalFollowing: {
+      type: Number,
+      default: 0,
+    },
+    followers: [
+      {
+        userName: {
+          type: String,
+          required: [false, "User ID?"],
+        },
+        userSavedIDs: [
+          {
+            postID: {
+              type: String,
+            },
+          },
+        ],
       },
     ],
     AccountCreatedAt: {
@@ -67,7 +70,7 @@ const userSchema = new mongoose.Schema({
       required: [true, "A account must have an email"],
       trim: true,
       lowercase: true,
-      validate: [validator.isEmail, 'Please enter a valid email address'],
+      validate: [validator.isEmail, "Please enter a valid email address"],
     },
     following: [
       {
@@ -97,7 +100,7 @@ const userSchema = new mongoose.Schema({
   },
   {
     collection: "users",
-  },
+  }
 );
 const User = mongoose.model("Users", userSchema);
 
